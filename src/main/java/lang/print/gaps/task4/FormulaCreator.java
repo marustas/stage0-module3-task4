1c967922812dfad653b70b1f948890962d6606cc
Stanislau Marudau <marudov.stas@gmail.com>
2022-06-27 21:24:42 +0300

    Fourth task first try

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a=1;
+        int b=2;
+        int c = (9*a^2-5*b+2+a+7)*(a+b-4*a*b)/2;
+        System.out.println(c);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,18 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int a = 97;
+        char a_char = (char)(a);
+        int c = 99;
+        char c_char =(char)(c);
+        int h = 104;
+        char h_char =(char)(h);
+        int r = 114;
+        char r_char =(char)(r);
+
+        System.out.println(c_char);
+        System.out.println(h_char);
+        System.out.println(a_char);
+        System.out.println(r_char);
     }
 }
